0e8b57b2ceb86a8145a3f4c7e9262f3b18c5f539
Rahul A <154581315+Rahuleey@users.noreply.github.com>
2024-05-28 17:09:44 +0530

    Update index.js

--- Congrats Card/index.js
+++ Congrats Card/index.js
@@ -1,15 +1,21 @@
 const element = (
-  //   Write your code here
-  <div>
-    <h1>Super Over League</h1>
-    <img
-      class='image'
-      src='https://assets.ccbp.in/frontend/react-js/rcb-img.png'
-    />
-    <img
-      class='image'
-      src='https://assets.ccbp.in/frontend/react-js/csk-img.png'
-    />
+  // Write your code here.
+  <div class='main'>
+    <div>
+      <h1 class='head'>Congratulations</h1>
+      <div class='cont'>
+        <img
+          class='image'
+          src='https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png'
+        />
+        <h3 class='head'>Kiran V</h3>
+        <p>Vishnu Institute of Computer Edducation and Technology Bhimavaram</p>
+        <img
+          class='image'
+          src='https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png'
+        />
+      </div>
+    </div>
   </div>
 )
 
